58fc4d8c0338ee9fed072e65201f88bfd4452216
Jukka Alavesa <juggeli@gmail.com>
2023-04-04 10:30:01 +0300

    update user confs

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,24 +1,26 @@
--- customize mason plugins
 return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      PATH = "append",
+    },
+  },
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = { "lua_ls", "rnix" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "stylua" },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
       -- ensure_installed = { "python" },
     },
